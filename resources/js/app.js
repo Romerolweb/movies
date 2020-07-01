89037c291dee7efb87cd8d6caf6b7b958e8117ee
Sebas Romero <srdesingweb@gmail.com>
2020-07-01 11:57:59 -0500

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,9 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-// import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
-// Vue.use(DropdownPlugin)
-// Vue.use(TablePlugin)
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
 
 require('./bootstrap');
 
